b99f7d9cb99095b22b4a12e736ab3db0fd039bd8
RomanKondr95 <139111155+RomanKondr95@users.noreply.github.com>
2023-07-25 15:18:53 +0300

    добавил возможность выбора даты и времени
    Добавил выбор даты и времени записи. Код требует корректировки в будущем.

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import telebot
 import sqlite3
 from telebot import types
 from tokennn import token
+from datetime import datetime,timedelta
 
 bot = telebot.TeleBot(token)
-# massage = 'массаж: 500'
-# spa = 'спа-капсула: 3000'
-# kolagen = 'коллагенарий: 2000'
+
+# услуги
 serv = {'массаж': 500, 'коллагенарий':2000, 'кедровая бочка':1000, 'спа-капсула':3000}
 massage = list(serv)[0]
 m_pr = serv[massage]
@@ -20,6 +20,10 @@ spa_pr = serv[spa]
 selected_serv = {}
 key = None
 value = None
+date = None
+time = None
+records = {}
+record = {}
 @bot.message_handler(commands=['start'])
 def start(message):
     conn = sqlite3.connect('database.sqlite')
@@ -30,7 +34,8 @@ create table if not exists USERS (
             lastname text,
             service text,
             price integer,
-            date text
+            date text,
+            time text
             
 )
             """)
@@ -71,12 +76,80 @@ def callback(call):
             selected_serv.setdefault(kedr,kedr_pr)
         if call.data == spa:
             selected_serv.setdefault(spa,spa_pr)
-            
+                        
     selected_services = ''        
     for key,value in selected_serv.items():
         selected_services += ''.join(f'{key} - {value}') + ', '
-    bot.send_message(call.message.chat.id, f'Вы выбрали: {selected_services[:-2]}')
+    bot.send_message(call.message.chat.id, f'Вы выбрали: {selected_services[:-2]}. Чтобы выбрать дату и время введите /запись либо добавьте еще услуги')
+    # bot.register_next_step_handler(call,handle_booking)
+
+# удалить
+@bot.message_handler(commands=['запись'])
+def handle_booking(message):
+    chat_id = message.chat.id
+    # создаем клавиатуру с выбором даты
+    markup = create_date_keyboard()
+    bot.send_message(chat_id, 'Укажите дату записи (в формате ДД.ММ.ГГГГ):', reply_markup=markup)
+
+# обработка ответа с выбранной датой
+@bot.message_handler(func=lambda message: True if message.text.count('.') == 2 else False)
+def handle_date(message):
+    chat_id = message.chat.id
+    date_str = message.text
+
+    try:
+        # преобразуем строку с датой в объект datetime
+        date = datetime.strptime(date_str, '%d.%m.%Y')
+        # создаем клавиатуру с выбором времени
+        markup = create_time_keyboard()
+        bot.send_message(chat_id, 'Укажите время записи (в формате ЧЧ:ММ):', reply_markup=markup)
+        # сохраняем запись в словаре
+        records[chat_id] = {'date': date, 'time': None}
+    except ValueError:
+        bot.send_message(chat_id, 'Неверный формат даты! Попробуйте еще раз.')
+
+# обработка ответа с выбранным временем
+@bot.message_handler(func=lambda message: True if ':' in message.text else False)
+def handle_time(message):
+    chat_id = message.chat.id
+    time_str = message.text
+
+    try:
+        # преобразуем строку с временем в объект datetime
+        time = datetime.strptime(time_str, '%H:%M').time()
+        # получаем запись из словаря
+        record = records.get(chat_id)
+        if record:
+            # устанавливаем выбранное время
+            record['time'] = time
+            bot.send_message(chat_id, 'Вы успешно записаны на {date} в {time}'.format(date=record['date'].strftime('%d.%m.%Y'), time=record['time'].strftime('%H:%M')),reply_markup=None)
+            print(record)
+    except ValueError:
+        bot.send_message(chat_id, 'Неверный формат времени! Попробуйте еще раз.',reply_markup=None)
     
+    
+# создание клавиатуры с выбором даты
+def create_date_keyboard():
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    today = datetime.now().date()
+    for i in range(7):
+        date = today + timedelta(days=i)
+        button_text = date.strftime('%d.%m.%Y')
+        markup.add(types.KeyboardButton(button_text))
+    return markup
+
+# создание клавиатуры с выбором времени
+def create_time_keyboard():
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    for hour in range(8, 21):
+        for minute in range(0, 60):
+            time = '{:02d}:{:02d}'.format(hour, minute)
+            markup.add(types.KeyboardButton(time))
+    return markup
+
+    
+
+
 
 
 
